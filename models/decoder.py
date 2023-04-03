888ddfb42fcb0515b8c7685e5548682ffb3a4c3c
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-04-03 18:33:00 +0000

    Creating an LSTM decoder

---
+++ models/decoder.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, LSTM, Dense, RepeatVector, TimeDistributed
+from tensorflow.keras.models import Model
+
+
+class Decoder:
+    """
+    Decoder class that creates a neural network model to decode a fixed-length representation (latent variable) into a
+    target sequence.
+    """
+    def __init__(self, n_sequence, n_features, n_latent):
+        """
+        Initializes the decoder model.
+
+        Parameters
+        ----------
+        n_sequence : int
+            The length of the target sequence.
+        n_features : int
+            The number of features in the target sequence.
+        n_latent : int
+            The size of the latent space (i.e., the dimensionality of the encoded representation).
+        """
+        latent_inputs = Input(shape=(n_latent,))
+        x = RepeatVector(n_sequence)(latent_inputs)
+        x = LSTM(32, return_sequences=True)(x)
+        output = TimeDistributed(Dense(n_features))(x)
+        self.model = Model(latent_inputs, output, name='Decoder-Model')
