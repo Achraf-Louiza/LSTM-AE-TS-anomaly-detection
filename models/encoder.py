bf383ed239d111729c901e2addf704495d2fb577
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-04-05 17:06:08 +0000

    Update encoder parameters

--- models/encoder.py
+++ models/encoder.py
@@ -8,20 +8,20 @@ class Encoder:
     Encoder class that creates a neural network model to encode input sequences into a fixed-length representation
     (latent variable) that can be used as input to a decoder network to generate a target sequence.
     """
-    def __init__(self, n_sequence, n_features, n_latent):
+    def __init__(self, length_sequence, n_features, n_latent):
         """ 
         Initializes the encoder model.
 
         Parameters
         ----------
-        n_sequence : int
+        length_sequence : int
             The length of the input sequence.
         n_features : int
             The number of features in the input sequence.
         n_latent : int
             The size of the latent space (i.e., the dimensionality of the encoded representation).
         """
-        inputs = Input(shape=(n_sequence, n_features))
+        inputs = Input(shape=(length_sequence, n_features))
         x = LSTM(32)(inputs)
         output = Dense(n_latent)(x)
         self.model = Model(inputs, output, name='Encoder-Model')
