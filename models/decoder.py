b928c71e26bf34702ec9e3930c54d6a129db66a2
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-04-05 17:06:26 +0000

    Update decoder parameters

--- models/decoder.py
+++ models/decoder.py
@@ -8,13 +8,13 @@ class Decoder:
     Decoder class that creates a neural network model to decode a fixed-length representation (latent variable) into a
     target sequence.
     """
-    def __init__(self, n_sequence, n_features, n_latent):
+    def __init__(self, length_sequence, n_features, n_latent):
         """
         Initializes the decoder model.
 
         Parameters
         ----------
-        n_sequence : int
+        length_sequence : int
             The length of the target sequence.
         n_features : int
             The number of features in the target sequence.
@@ -22,7 +22,7 @@ class Decoder:
             The size of the latent space (i.e., the dimensionality of the encoded representation).
         """
         latent_inputs = Input(shape=(n_latent,))
-        x = RepeatVector(n_sequence)(latent_inputs)
+        x = RepeatVector(length_sequence)(latent_inputs)
         x = LSTM(32, return_sequences=True)(x)
         output = TimeDistributed(Dense(n_features))(x)
         self.model = Model(latent_inputs, output, name='Decoder-Model')
